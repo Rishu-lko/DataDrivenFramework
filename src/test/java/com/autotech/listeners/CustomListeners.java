411765acd22515a2e649e6647b5f324efa3cdbb3
unknown <HP@HCL-IND-LKO-L47.brightstarcorp.com>
2019-05-03 22:33:43 +0530

    Added ReportNG Modifications

--- src/test/java/com/autotech/listeners/CustomListeners.java
+++ src/test/java/com/autotech/listeners/CustomListeners.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.Date;
 
 import org.testng.Assert;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
@@ -36,6 +37,42 @@ public class CustomListeners extends TestBase implements ITestListener {
 			e.printStackTrace();
 			Assert.fail("Unable to capture error screenshot");
 		}
+	}
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
